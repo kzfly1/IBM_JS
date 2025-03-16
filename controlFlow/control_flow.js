b25c5908a0814e53a9a5b35cf02d489ec754e874
kzfly1 <qichenxu47@gmail.com>
2025-03-16 12:18:39 -0400

    message

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -44,4 +44,8 @@ switch(userType) {
         userCategory = "Unknown";
 }
 
-console.log("User Category:", userCategory)
+console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+console.log("Authentication Status:", authenticationStatus);
